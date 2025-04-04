9628837ebdd765ff706439f904e6be05637f4084
Tomy <tomywang999@gmail.com>
2025-04-04 13:46:33 +0800

    3 spec test

---
+++ dist-electron/main.js
@@ -0,0 +1,36 @@
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const url = require("url");
+var _documentCurrentScript = typeof document !== "undefined" ? document.currentScript : null;
+const __filename$1 = url.fileURLToPath(typeof document === "undefined" ? require("url").pathToFileURL(__filename).href : _documentCurrentScript && _documentCurrentScript.tagName.toUpperCase() === "SCRIPT" && _documentCurrentScript.src || new URL("main.js", document.baseURI).href);
+const __dirname$1 = path.dirname(__filename$1);
+function createWindow() {
+  const win = new electron.BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+  if (process.env.NODE_ENV === "development") {
+    win.loadURL("http://localhost:5173");
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname$1, "../dist/index.html"));
+  }
+}
+electron.app.whenReady().then(() => {
+  createWindow();
+  electron.app.on("activate", () => {
+    if (electron.BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+electron.app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    electron.app.quit();
+  }
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import electron from 'vite-plugin-electron'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    electron({
+      entry: 'electron/main.js'
+    })
+  ],
+  base: process.env.NODE_ENV === 'development' ? '/' : './',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
